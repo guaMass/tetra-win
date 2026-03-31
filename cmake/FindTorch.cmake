58108b96278be935caf064f816ce99594c133f82
Meng Wugang <710078826@qq.com>
2026-03-31 11:57:41 +0800

    Fix CUDA module compatibility for Torch CMake

--- cmake/FindTorch.cmake
+++ cmake/FindTorch.cmake
@@ -1,34 +1,72 @@
+# Finds PyTorch from the active Python environment and exposes:
+#   TORCH_LIBRARIES
+#   TORCH_INCLUDE_DIRS
+#   TORCH_LIBRARY_DIRS
+#   TORCH_COMPILE_OPTIONS
+#   TORCH_COMPILE_DEFINITIONS
+
 if(NOT DEFINED PYTHON_EXECUTABLE)
-  execute_process(COMMAND python -c "import sys; print(sys.executable,end='')" OUTPUT_VARIABLE PYTHON_EXECUTABLE)
-  execute_process(COMMAND "${PYTHON_EXECUTABLE}" --version OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE TORCH_PYTHON_VERSION)
-  message(STATUS "Using ${TORCH_PYTHON_VERSION}")
-  execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c "import torch; print(f'{torch.__version__}',end='')" RESULT_VARIABLE STATUS OUTPUT_VARIABLE TORCH_VERSION)
-  if(STATUS AND NOT STATUS EQUAL 0)
-    message(FATAL_ERROR "Could not find torch library using python path: ${PYTHON_EXECUTABLE}")
-  endif()
-  message(STATUS "Found torch ${TORCH_VERSION}")
-endif()
-if(NOT DEFINED CUDA_TOOLKIT_ROOT_DIR)
-  execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c "import torch.utils.cpp_extension; print(f'{torch.utils.cpp_extension.CUDA_HOME}',end='')" RESULT_VARIABLE STATUS OUTPUT_VARIABLE CUDA_TOOLKIT_ROOT_DIR)
-endif()
-if(NOT DEFINED TORCH_LIBRARY_DIRS)
-  execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c "import torch.utils.cpp_extension; print(';'.join(torch.utils.cpp_extension.library_paths(False)),end='')" COMMAND_ERROR_IS_FATAL ANY OUTPUT_VARIABLE TORCH_LIBRARY_DIRS)
-endif()
-if(NOT DEFINED TORCH_INCLUDE_DIRS)
-  execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c "import torch.utils.cpp_extension; print(';'.join(torch.utils.cpp_extension.include_paths(False)),end='')" COMMAND_ERROR_IS_FATAL ANY OUTPUT_VARIABLE TORCH_INCLUDE_DIRS)
+  find_package(Python3 COMPONENTS Interpreter REQUIRED)
+  set(PYTHON_EXECUTABLE "${Python3_EXECUTABLE}")
 endif()
 
-if("$ENV{CUDAARCHS}" STREQUAL "")
-  execute_process(COMMAND "${PYTHON_EXECUTABLE}" -c "import torch.utils.cpp_extension;print(' '.join(sorted(set(x.split('_')[-1] for x in torch.utils.cpp_extension._get_cuda_arch_flags()))),end='')" COMMAND_ERROR_IS_FATAL ANY OUTPUT_VARIABLE CMAKE_CUDA_ARCHITECTURES)
+execute_process(
+  COMMAND "${PYTHON_EXECUTABLE}" -c "import torch; print(torch.__version__, end='')"
+  RESULT_VARIABLE _torch_status
+  OUTPUT_VARIABLE TORCH_VERSION
+)
+if(NOT _torch_status EQUAL 0)
+  message(FATAL_ERROR "Could not import torch using Python executable: ${PYTHON_EXECUTABLE}")
 endif()
 
-# Setup everything
-message(STATUS "Using torch libraries: ${TORCH_LIBRARY_DIRS}")
-message(STATUS "Using torch includes: ${TORCH_INCLUDE_DIRS}")
-message(STATUS "Using CUDA toolkit: ${CUDA_TOOLKIT_ROOT_DIR}")
-message(STATUS "Using CUDA architectures: ${CMAKE_CUDA_ARCHITECTURES}")
-include_directories(${TORCH_INCLUDE_DIRS})
-link_directories(${TORCH_LIBRARY_DIRS})
-set(TORCH_LIBRARIES c10 torch torch_cpu torch_python)
-string(APPEND CMAKE_CUDA_FLAGS " -D__CUDA_NO_HALF_OPERATORS__ -D__CUDA_NO_HALF_CONVERSIONS__ -D__CUDA_NO_HALF2_OPERATORS__ --expt-relaxed-constexpr --compiler-options '-fPIC' -O3 --use_fast_math")
-string(APPEND CMAKE_CXX_FLAGS " -Wno-deprecated-declarations -D_GLIBCXX_USE_CXX11_ABI=0")
+execute_process(
+  COMMAND "${PYTHON_EXECUTABLE}" -c "import torch.utils; print(torch.utils.cmake_prefix_path, end='')"
+  COMMAND_ERROR_IS_FATAL ANY
+  OUTPUT_VARIABLE _torch_cmake_prefix
+)
+list(APPEND CMAKE_PREFIX_PATH "${_torch_cmake_prefix}")
+
+find_package(Torch CONFIG REQUIRED)
+
+execute_process(
+  COMMAND "${PYTHON_EXECUTABLE}" -c "import torch.utils.cpp_extension as ext; print(';'.join(ext.include_paths(False)), end='')"
+  COMMAND_ERROR_IS_FATAL ANY
+  OUTPUT_VARIABLE TORCH_INCLUDE_DIRS
+)
+execute_process(
+  COMMAND "${PYTHON_EXECUTABLE}" -c "import torch.utils.cpp_extension as ext; print(';'.join(ext.library_paths(False)), end='')"
+  COMMAND_ERROR_IS_FATAL ANY
+  OUTPUT_VARIABLE TORCH_LIBRARY_DIRS
+)
+
+set(_torch_config_libraries "${TORCH_LIBRARIES}")
+set(TORCH_LIBRARIES)
+if(TARGET Torch::Torch)
+  list(APPEND TORCH_LIBRARIES Torch::Torch)
+endif()
+if(TARGET Torch::Python)
+  list(APPEND TORCH_LIBRARIES Torch::Python)
+endif()
+if(NOT TORCH_LIBRARIES AND NOT _torch_config_libraries STREQUAL "")
+  set(TORCH_LIBRARIES ${_torch_config_libraries})
+endif()
+if(NOT TORCH_LIBRARIES)
+  message(FATAL_ERROR "Torch was found, but no usable CMake targets were exported.")
+endif()
+
+set(TORCH_COMPILE_OPTIONS)
+if(DEFINED TORCH_CXX_FLAGS AND NOT TORCH_CXX_FLAGS STREQUAL "")
+  separate_arguments(_torch_cxx_flags NATIVE_COMMAND "${TORCH_CXX_FLAGS}")
+  list(APPEND TORCH_COMPILE_OPTIONS ${_torch_cxx_flags})
+endif()
+
+set(TORCH_COMPILE_DEFINITIONS
+  __CUDA_NO_HALF_OPERATORS__
+  __CUDA_NO_HALF_CONVERSIONS__
+  __CUDA_NO_HALF2_OPERATORS__
+)
+
+message(STATUS "Found torch ${TORCH_VERSION}")
+message(STATUS "Using torch cmake prefix: ${_torch_cmake_prefix}")
+message(STATUS "Using torch include dirs: ${TORCH_INCLUDE_DIRS}")
+message(STATUS "Using torch library dirs: ${TORCH_LIBRARY_DIRS}")
